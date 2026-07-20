66dfe66b23eef4c825498ab866bf09e787d1fd9e
Vladislav-jpg97 <sabiwaaa17@gmail.com>
2026-07-20 14:16:17 +0500

    feat (HW_lesson-5-part-2):added model recipe review

--- backend/src/backend/models/__init__.py
+++ backend/src/backend/models/__init__.py
@@ -2,4 +2,5 @@ from backend.models.base import Base
 from backend.models.models import Recipe
 from backend.models.cuisine import Cuisine
 from backend.models.ingredient import Ingredient,recipe_ingredients
-__all__ = ["Base","Recipe","Cuisine"]
+from backend.models.recipe_review import RecipeReview
+__all__ = ["Base","Recipe","Cuisine","Ingredient","RecipeReview","recipe_ingredients"]
