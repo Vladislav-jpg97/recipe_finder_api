d3ec106d0d9bc5a9a0e14341689c6bec39449986
Vladislav-jpg97 <sabiwaaa17@gmail.com>
2026-07-24 10:01:25 +0500

    feat (Lessont-7-HW-part-2) : added model User and create new migration alembic

--- backend/src/backend/core/config.py
+++ backend/src/backend/core/config.py
@@ -11,8 +11,8 @@ class Settings(BaseSettings):
     database_url: str
     secret_key: str
     algorithm : str
-    access_token: str
-    refresh_token: str
+    access_token_expire_minutes: int
+    refresh_token_expire_days: int
 
     model_config = SettingsConfigDict(
         env_file=BASE_DIR / ".env",
--- backend/src/backend/models/__init__.py
+++ backend/src/backend/models/__init__.py
@@ -3,4 +3,5 @@ from backend.models.models import Recipe
 from backend.models.cuisine import Cuisine
 from backend.models.ingredient import Ingredient,recipe_ingredients
 from backend.models.recipe_review import RecipeReview
-__all__ = ["Base","Recipe","Cuisine","Ingredient","RecipeReview","recipe_ingredients"]
+from backend.models.user import User
+__all__ = ["Base","Recipe","Cuisine","Ingredient","RecipeReview","recipe_ingredients","User"]
